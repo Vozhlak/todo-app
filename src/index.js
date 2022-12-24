1be2f6e6b0cbf1fd67a963ae551f3e45aef7d0eb
vozhlak <vozhlakov1997@yandex.ru>
2022-12-24 23:25:20 +0600

    add refactoring index and task-list

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const str = '';
-const str1 = '';
-const str2 = '';
 root.render(
   <React.StrictMode>
     <App />
