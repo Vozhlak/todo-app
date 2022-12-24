468d76feb491309d4a629798b178ed3c3357229a
vozhlak <vozhlakov1997@yandex.ru>
2022-12-24 23:15:14 +0600

    add eslintrc and prettierrc config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+const str = ''
 root.render(
   <React.StrictMode>
     <App />
